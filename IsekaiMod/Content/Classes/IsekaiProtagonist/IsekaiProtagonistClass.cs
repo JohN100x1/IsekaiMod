8eb1f72e503e0bca7922e593024c957b1d6f84fa
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-12-01 21:52:48 +0000

    feat: update CHANGELOG.md

--- IsekaiMod/Content/Classes/IsekaiProtagonist/IsekaiProtagonistClass.cs
+++ IsekaiMod/Content/Classes/IsekaiProtagonist/IsekaiProtagonistClass.cs
@@ -30,8 +30,7 @@ namespace IsekaiMod.Content.Classes.IsekaiProtagonist
 
         public static void Add()
         {
-            // TODO: rename corrupt aura and protectice aura
-            // TODO: add scaling natural armor, strength and dexterity (like animal companion) to deathsnatcher
+            // TODO: add scaling natural armor, strength and dexterity (like animal companion) to deathsnatcher?
             // TODO: add vampiric drain spell for isekai vampire heritage?
             // TODO: Load localisation instead of hardcoded strings
 
