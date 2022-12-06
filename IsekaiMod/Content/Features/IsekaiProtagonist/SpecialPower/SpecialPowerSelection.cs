aeea222768c9dde7c3c41fc18d5d7de53a145008
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-12-06 01:25:41 +0000

    feat: update special power icon

--- IsekaiMod/Content/Features/IsekaiProtagonist/SpecialPower/SpecialPowerSelection.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/SpecialPower/SpecialPowerSelection.cs
@@ -3,20 +3,21 @@ using IsekaiMod.Utilities;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Classes.Selection;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
 using UnityEngine;
 
 namespace IsekaiMod.Content.Features.IsekaiProtagonist.SpecialPower
 {
     class SpecialPowerSelection
     {
-        private static readonly Sprite Icon_Discovery = Resources.GetBlueprint<BlueprintFeatureSelection>("cd86c437488386f438dcc9ae727ea2a6").m_Icon;
+        private static readonly Sprite Icon_ForetellAidBuff = Resources.GetBlueprint<BlueprintBuff>("faf473e3a977fd4428cd3f1a526346d2").m_Icon;
         public static void Add()
         {
             var IsekaiProtagonistTalentSelection = Resources.GetModBlueprint<BlueprintFeatureSelection>("IsekaiProtagonistTalentSelection");
             var SpecialPowerSelection = Helpers.CreateBlueprint<BlueprintFeatureSelection>("SpecialPowerSelection", bp => {
                 bp.SetName("Special Power");
                 bp.SetDescription("As you increase your level, you gain special powers that allow you to wreck your enemies more easily.");
-                bp.m_Icon = Icon_Discovery;
+                bp.m_Icon = Icon_ForetellAidBuff;
                 bp.Ranks = 1;
                 bp.IsClassFeature = true;
 
