8eda79286213ad150bef186521a8c95b5827447b
kjk001 <kjkaergel@gmail.com>
2023-02-19 11:29:46 +0100

    2 blueprints had their id changed and blessing added to special powers as well

--- IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/BlessingOfTheMythic.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/BlessingOfTheMythic.cs
@@ -178,6 +178,7 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.OverpoweredAbility {
                 };
             });
             OverpoweredAbilitySelection.AddToSelection(MythicSelection);
+            SpecialPower.SpecialPowerSelection.AddToSelection(MythicSelection);
         }
     }
 }
