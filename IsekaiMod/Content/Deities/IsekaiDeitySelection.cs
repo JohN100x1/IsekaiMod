941834bf0633421d6481bdb73f465c841af13be1
JohN100x1 <JohN100x1@hotmail.co.uk>
2023-03-11 02:38:19 +0000

    fix: deity selection

--- IsekaiMod/Content/Deities/IsekaiDeitySelection.cs
+++ IsekaiMod/Content/Deities/IsekaiDeitySelection.cs
@@ -4,45 +4,46 @@ using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Classes.Selection;
 using Kingmaker.Utility;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using TabletopTweaks.Core.Utilities;
 using static IsekaiMod.Main;
 
 namespace IsekaiMod.Content.Deities {
+
     internal class IsekaiDeitySelection {
         private static BlueprintFeatureSelection DeitySelection = BlueprintTools.GetBlueprint<BlueprintFeatureSelection>("59e7a76987fe3b547b9cce045f4db3e4");
+
         public static void Add() {
             // Isekai Background Selection
             var IsekaiDeitySelection = Helpers.CreateBlueprint<BlueprintFeatureSelection>(IsekaiContext, "IsekaiDeitySelection", bp => {
                 bp.SetName(IsekaiContext, "Isekai");
                 bp.SetDescription(IsekaiContext, "Gods from another world.");
-                bp.m_Icon = null;
-                bp.m_AllFeatures = new BlueprintFeatureReference[] { }; // Added upon blueprint creation
+                bp.m_AllFeatures = new BlueprintFeatureReference[0]; // Added upon blueprint creation
                 bp.Groups = new FeatureGroup[] { FeatureGroup.Deities };
                 bp.Group = FeatureGroup.Deities;
             });
 
             // Add to Deity Selection
-            
-            DeitySelection.AddFeatures(IsekaiDeitySelection);
+            // DeitySelection.AddFeatures(IsekaiDeitySelection);
+            DeitySelection.m_AllFeatures = DeitySelection.m_AllFeatures.AddToArray(IsekaiDeitySelection.ToReference<BlueprintFeatureReference>());
         }
 
-        /** wasn't us that broke it but I am adding the patch here anyway 
+        /** wasn't us that broke it but I am adding the patch here anyway
          * because one of my other mods seriously messed with this and created a recursion that adds the selection to itself and every subselection and it is really irritating because of bloating,
          * so we might as well be the good guys and fix it for them */
-        public static void PatchDeitySelection() {       
-            
+
+        public static void PatchDeitySelection() {
             var myDeityList = new Dictionary<int, HashSet<BlueprintFeatureSelection>>();
-            var levelList = new HashSet<BlueprintFeatureSelection>();
-            levelList.Add(DeitySelection);
-            myDeityList.Add(1,levelList);
+            var levelList = new HashSet<BlueprintFeatureSelection> {
+                DeitySelection
+            };
+            myDeityList.Add(1, levelList);
             var knownDeities = new HashSet<BlueprintFeature>();
             FlattenSelection(myDeityList, knownDeities, 1);
-            
+
             //if (IsekaiContext.AddedContent.Deities.IsDisabled("Isekai Deities")) return;
             //any potential god patches that are not the above bugfix should go here, after commenting the if clause in
         }
+
         private static bool isKnownSelection(BlueprintFeatureSelection selection, Dictionary<int, HashSet<BlueprintFeatureSelection>> knownselections) {
             foreach (var key in knownselections.Keys) {
                 foreach (var checkselection in knownselections.Get(key)) { if (checkselection.AssetGuid == selection.AssetGuid) { return true; } }
@@ -64,7 +65,6 @@ namespace IsekaiMod.Content.Deities {
                             } else {
                                 nextLevel.Add(selection2);
                             }
-
                         } else {
                             if (knownDeities.Contains(unRef)) {
                                 //IsekaiContext.Logger.Log("Duplicate Deity Entry=" + unRef.AssetGuid);
@@ -74,19 +74,19 @@ namespace IsekaiMod.Content.Deities {
                             }
                         }
                     } else {
-                        IsekaiContext.Logger.Log("Null Value in selection of="+selection.AssetGuid);
+                        IsekaiContext.Logger.Log("Null Value in selection of=" + selection.AssetGuid);
                     }
                 }
             }
-            if (nextLevel.Count == 0) { 
-                return; 
+            if (nextLevel.Count == 0) {
+                return;
             }
             level++;
             if (level > 10) {
                 IsekaiContext.Logger.LogError("Deity List Patching has reached 10 levels of depth, emergency aborting attempt to fix the levels, even with recursion this should not be possible unless someone purposfully fucked with the deity list, please doublecheck ingame");
                 return;
             }
-            knownselections.Add(level,nextLevel);
+            knownselections.Add(level, nextLevel);
             FlattenSelection(knownselections, knownDeities, level);
         }
 
