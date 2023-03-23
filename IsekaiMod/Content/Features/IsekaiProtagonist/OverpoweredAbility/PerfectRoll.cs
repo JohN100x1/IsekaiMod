e7c63e039abe3e34f91a19dad17e469325932f16
JohN100x1 <JohN100x1@hotmail.co.uk>
2023-03-23 19:24:09 +0000

    feat: update meta luck OP ability

--- IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/PerfectRoll.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/PerfectRoll.cs
@@ -5,6 +5,8 @@ using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
 using Kingmaker.UnitLogic.Buffs.Blueprints;
 using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using Kingmaker.UnitLogic.Mechanics.Components;
 using TabletopTweaks.Core.Utilities;
 using UnityEngine;
 using static IsekaiMod.Main;
@@ -15,28 +17,47 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.OverpoweredAbility {
         private static readonly Sprite Icon_Fortune = BlueprintTools.GetBlueprint<BlueprintAbility>("eaf7077a8ff35644883df6d4f7b2084c").m_Icon;
 
         public static void Add() {
+            var MetaLuckDebuff = ThingsNotHandledByTTTCore.CreateBuff("MetaLuckDebuff", bp => {
+                bp.SetName(IsekaiContext, "Overpowered Ability — Meta Luck");
+                bp.SetDescription(IsekaiContext, "This creature takes the lower of two d20 rolls.");
+                bp.m_Icon = Icon_Fortune;
+                bp.AddComponent<ModifyD20>(c => {
+                    c.Rule = RuleType.All;
+                    c.RollsAmount = 1;
+                });
+                bp.Stacking = StackingType.Replace;
+                bp.IsClassFeature = true;
+            });
             var MetaLuckBuff = ThingsNotHandledByTTTCore.CreateBuff("MetaLuckBuff", bp => {
                 bp.SetName(IsekaiContext, "Overpowered Ability — Meta Luck");
-                bp.SetDescription(IsekaiContext, "This character always takes the higher of three d20 rolls.");
+                bp.SetDescription(IsekaiContext, "This character always takes the higher of two d20 rolls.");
                 bp.m_Icon = Icon_Fortune;
                 bp.AddComponent<ModifyD20>(c => {
                     c.Rule = RuleType.All;
                     c.RollsAmount = 1;
                     c.TakeBest = true;
                 });
+                bp.AddComponent<AddTargetBeforeAttackRollTrigger>(c => {
+                    c.ActionsOnAttacker = ActionFlow.DoSingle<ContextActionApplyBuff>(c => {
+                        c.m_Buff = MetaLuckDebuff.ToReference<BlueprintBuffReference>();
+                        c.DurationValue = Values.Duration.OneRound;
+                    });
+                    c.ActionOnSelf = ActionFlow.DoNothing();
+                });
                 bp.Stacking = StackingType.Replace;
                 bp.IsClassFeature = true;
-                bp.m_Flags = BlueprintBuff.Flags.StayOnDeath;
             });
             var MetaLuckAbility = ThingsNotHandledByTTTCore.CreateActivatableAbility("MetaLuckAbility", bp => {
                 bp.SetName(IsekaiContext, "Overpowered Ability — Meta Luck");
-                bp.SetDescription(IsekaiContext, "You always take the higher of three d20 rolls.");
+                bp.SetDescription(IsekaiContext, "You always take the higher of two d20 rolls."
+                    + "\nEnemies that attack you take the lower of two d20 rolls for one round.");
                 bp.m_Icon = Icon_Fortune;
                 bp.m_Buff = MetaLuckBuff.ToReference<BlueprintBuffReference>();
             });
             var MetaLuckFeature = Helpers.CreateBlueprint<BlueprintFeature>(IsekaiContext, "MetaLuckFeature", bp => {
                 bp.SetName(IsekaiContext, "Overpowered Ability — Meta Luck");
-                bp.SetDescription(IsekaiContext, "You always take the higher of three d20 rolls.");
+                bp.SetDescription(IsekaiContext, "You always take the higher of two d20 rolls."
+                    + "\nEnemies that attack you take the lower of two d20 rolls for one round.");
                 bp.m_Icon = Icon_Fortune;
                 bp.AddComponent<AddFacts>(c => {
                     c.m_Facts = new BlueprintUnitFactReference[] { MetaLuckAbility.ToReference<BlueprintUnitFactReference>() };
