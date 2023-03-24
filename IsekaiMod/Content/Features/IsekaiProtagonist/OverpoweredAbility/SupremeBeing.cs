254da5c2b6bdb0cb49920b3b3d70b207082d6c45
JohN100x1 <JohN100x1@hotmail.co.uk>
2023-03-24 21:42:18 +0000

    feat: update description for supreme being

--- IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/SupremeBeing.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/SupremeBeing.cs
@@ -14,7 +14,9 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.OverpoweredAbility {
         public static void Add() {
             var SupremeBeing = Helpers.CreateBlueprint<BlueprintFeature>(IsekaiContext, "SupremeBeing", bp => {
                 bp.SetName(IsekaiContext, "Overpowered Ability — Supreme Being");
-                bp.SetDescription(IsekaiContext, "All your attributes have at least a base value of 30.");
+                bp.SetDescription(IsekaiContext, "You have forgotten the hardships of mortality and what sort of person you were before. "
+                    + "Were you stupid? Were you weak? It matters no longer. Here in this new world, you are perfect."
+                    + "\nBenefit: All your attributes have at least a base value of 30.");
                 bp.m_Icon = Icon_KiPowerSelection;
                 bp.AddComponent<SetBaseStat>(c => {
                     c.Stat = StatType.Strength;
