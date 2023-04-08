55248d95aa8a364e0f31f2dc035a01d4631e2ef6
JohN100x1 <JohN100x1@hotmail.co.uk>
2023-04-09 00:11:07 +0100

    feat: update chosen path to transform full damage

--- IsekaiMod/Content/Features/IsekaiProtagonist/Archetypes/GodEmperor/PathSelection.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/Archetypes/GodEmperor/PathSelection.cs
@@ -1,12 +1,10 @@
-﻿using IsekaiMod.Components;
-using IsekaiMod.Utilities;
+﻿using IsekaiMod.Utilities;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
-using Kingmaker.Blueprints.Classes.Prerequisites;
 using Kingmaker.Blueprints.Classes.Selection;
 using Kingmaker.Enums.Damage;
+using Kingmaker.RuleSystem.Rules.Damage;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
-using Kingmaker.UnitLogic.Alignments;
 using Kingmaker.UnitLogic.FactLogic;
 using TabletopTweaks.Core.Utilities;
 using static IsekaiMod.Main;
@@ -17,15 +15,20 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.Archetypes.GodEmperor {
 
         public static void Add() {
             const string RighteousPathName = "Righteous Path";
-            const string RighteousPathDescription = "You transform half the damage you deal into divine damage.";
+            const string RighteousPathDescription = "The damage you deal is now divine damage.";
             var Icon_RighteousPath = BlueprintTools.GetBlueprint<BlueprintAbility>("0f5bd128c76dd374b8cb9111e3b5186b").m_Icon;
             var RighteousPathBuff = TTCoreExtensions.CreateBuff("RighteousPathBuff", bp => {
                 bp.SetName(IsekaiContext, RighteousPathName);
                 bp.SetDescription(IsekaiContext, RighteousPathDescription);
                 bp.IsClassFeature = true;
                 bp.m_Icon = Icon_RighteousPath;
-                bp.AddComponent<SplitOutgoingDamage>(c => {
-                    c.Element = DamageEnergyType.Divine;
+                bp.AddComponent<ChangeOutgoingDamageType>(c => {
+                    c.Type = new DamageTypeDescription() {
+                        Type = DamageType.Energy,
+                        Common = new DamageTypeDescription.CommomData(),
+                        Physical = new DamageTypeDescription.PhysicalData(),
+                        Energy = DamageEnergyType.Divine
+                    };
                 });
             });
             var RighteousPathAbility = TTCoreExtensions.CreateActivatableAbility("RighteousPathAbility", bp => {
@@ -45,15 +48,20 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.Archetypes.GodEmperor {
             });
 
             const string UnjustPathName = "Unjust Path";
-            const string UnjustPathDescription = "You transform half the damage you deal into unholy damage.";
+            const string UnjustPathDescription = "The damage you deal is now unholy damage.";
             var Icon_UnjustPath = AssetLoader.LoadInternal(IsekaiContext, "Features", "ICON_UNJUST_PATH.png");
             var UnjustPathBuff = TTCoreExtensions.CreateBuff("UnjustPathBuff", bp => {
                 bp.SetName(IsekaiContext, UnjustPathName);
                 bp.SetDescription(IsekaiContext, UnjustPathDescription);
                 bp.IsClassFeature = true;
                 bp.m_Icon = Icon_UnjustPath;
-                bp.AddComponent<SplitOutgoingDamage>(c => {
-                    c.Element = DamageEnergyType.Unholy;
+                bp.AddComponent<ChangeOutgoingDamageType>(c => {
+                    c.Type = new DamageTypeDescription() {
+                        Type = DamageType.Energy,
+                        Common = new DamageTypeDescription.CommomData(),
+                        Physical = new DamageTypeDescription.PhysicalData(),
+                        Energy = DamageEnergyType.Unholy
+                    };
                 });
             });
             var UnjustPathAbility = TTCoreExtensions.CreateActivatableAbility("UnjustPathAbility", bp => {
@@ -74,7 +82,7 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.Archetypes.GodEmperor {
 
             var PathSelection = Helpers.CreateBlueprint<BlueprintFeatureSelection>(IsekaiContext, "PathSelection", bp => {
                 bp.SetName(IsekaiContext, "Chosen Path");
-                bp.SetDescription(IsekaiContext, "At 12th level, you transform half the damage you deal into either divine or unholy.");
+                bp.SetDescription(IsekaiContext, "At 12th level, you transform the damage you deal into either divine or unholy.");
                 bp.m_Icon = Icon_RighteousPath;
                 bp.IgnorePrerequisites = true;
                 bp.m_AllFeatures = new BlueprintFeatureReference[] {
