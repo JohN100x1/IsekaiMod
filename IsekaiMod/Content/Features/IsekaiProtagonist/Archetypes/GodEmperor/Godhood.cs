9af9730f14ae54c9fd2c0cbf7653977f3f35c949
JohN100x1 <JohN100x1@hotmail.co.uk>
2023-04-10 16:21:24 +0100

    feat: update godly vessel and godhood

--- IsekaiMod/Content/Features/IsekaiProtagonist/Archetypes/GodEmperor/Godhood.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/Archetypes/GodEmperor/Godhood.cs
@@ -2,7 +2,6 @@
 using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Classes.Spells;
 using Kingmaker.Designers.Mechanics.Facts;
-using Kingmaker.Enums.Damage;
 using Kingmaker.UnitLogic.FactLogic;
 using TabletopTweaks.Core.Utilities;
 using static IsekaiMod.Main;
@@ -16,57 +15,12 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.Archetypes.GodEmperor {
             var Godhood = Helpers.CreateBlueprint<BlueprintFeature>(IsekaiContext, "Godhood", bp => {
                 bp.SetName(IsekaiContext, "Godhood");
                 bp.SetDescription(IsekaiContext, "At 20th level, you surpass the pinnacle of this physical reality. "
-                    + "You are immune to acid, cold, electricity, fire, sonic, spells and {g|Encyclopedia:Physical_Damage}physical damage{/g}. "
+                    + "You are immune to all spells and {g|Encyclopedia:Physical_Damage}physical damage{/g}. "
                     + "Your attacks ignore concealment and damage reduction. Your spells ignore spell resistance and spell immunity.");
                 bp.m_Icon = Icon_Godhood;
                 bp.AddComponent<AddSpellImmunity>();
                 bp.AddComponent<AreaEffectImmunity>();
                 bp.AddComponent<AddPhysicalImmunity>();
-                bp.AddComponent<AddEnergyImmunity>(c => {
-                    c.Type = DamageEnergyType.Acid;
-                });
-                bp.AddComponent<AddEnergyImmunity>(c => {
-                    c.Type = DamageEnergyType.Cold;
-                });
-                bp.AddComponent<AddEnergyImmunity>(c => {
-                    c.Type = DamageEnergyType.Electricity;
-                });
-                bp.AddComponent<AddEnergyImmunity>(c => {
-                    c.Type = DamageEnergyType.Fire;
-                });
-                bp.AddComponent<AddEnergyImmunity>(c => {
-                    c.Type = DamageEnergyType.Sonic;
-                });
-                bp.AddComponent<BuffDescriptorImmunity>(c => {
-                    c.Descriptor = SpellDescriptor.Acid;
-                });
-                bp.AddComponent<BuffDescriptorImmunity>(c => {
-                    c.Descriptor = SpellDescriptor.Cold;
-                });
-                bp.AddComponent<BuffDescriptorImmunity>(c => {
-                    c.Descriptor = SpellDescriptor.Electricity;
-                });
-                bp.AddComponent<BuffDescriptorImmunity>(c => {
-                    c.Descriptor = SpellDescriptor.Fire;
-                });
-                bp.AddComponent<BuffDescriptorImmunity>(c => {
-                    c.Descriptor = SpellDescriptor.Sonic;
-                });
-                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
-                    c.Descriptor = SpellDescriptor.Acid;
-                });
-                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
-                    c.Descriptor = SpellDescriptor.Cold;
-                });
-                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
-                    c.Descriptor = SpellDescriptor.Electricity;
-                });
-                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
-                    c.Descriptor = SpellDescriptor.Fire;
-                });
-                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
-                    c.Descriptor = SpellDescriptor.Sonic;
-                });
                 bp.AddComponent<IgnoreConcealment>();
                 bp.AddComponent<IgnoreDamageReductionOnAttack>();
                 bp.AddComponent<IgnoreSpellImmunity>(c => {
--- IsekaiMod/Content/Features/IsekaiProtagonist/Archetypes/GodEmperor/GodlyVessel.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/Archetypes/GodEmperor/GodlyVessel.cs
@@ -1,6 +1,6 @@
 ﻿using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Classes.Spells;
-using Kingmaker.UnitLogic;
+using Kingmaker.Enums.Damage;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
 using Kingmaker.UnitLogic.FactLogic;
 using TabletopTweaks.Core.Utilities;
@@ -14,9 +14,53 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.Archetypes.GodEmperor {
             var Icon_PureForm = BlueprintTools.GetBlueprint<BlueprintAbility>("33e53b74891b4c34ba6ee3baa322beeb").m_Icon;
             var GodlyVessel = Helpers.CreateBlueprint<BlueprintFeature>(IsekaiContext, "GodlyVessel", bp => {
                 bp.SetName(IsekaiContext, "Godly Vessel");
-                bp.SetDescription(IsekaiContext, "At 17th level, the God Emperor gains immunity to ..."
-                    );
+                bp.SetDescription(IsekaiContext, "At 17th level, the God Emperor gains immunity to all elements (acid, cold, electricity, fire, and sonic).");
                 bp.m_Icon = Icon_PureForm;
+                bp.AddComponent<AddEnergyImmunity>(c => {
+                    c.Type = DamageEnergyType.Acid;
+                });
+                bp.AddComponent<AddEnergyImmunity>(c => {
+                    c.Type = DamageEnergyType.Cold;
+                });
+                bp.AddComponent<AddEnergyImmunity>(c => {
+                    c.Type = DamageEnergyType.Electricity;
+                });
+                bp.AddComponent<AddEnergyImmunity>(c => {
+                    c.Type = DamageEnergyType.Fire;
+                });
+                bp.AddComponent<AddEnergyImmunity>(c => {
+                    c.Type = DamageEnergyType.Sonic;
+                });
+                bp.AddComponent<BuffDescriptorImmunity>(c => {
+                    c.Descriptor = SpellDescriptor.Acid;
+                });
+                bp.AddComponent<BuffDescriptorImmunity>(c => {
+                    c.Descriptor = SpellDescriptor.Cold;
+                });
+                bp.AddComponent<BuffDescriptorImmunity>(c => {
+                    c.Descriptor = SpellDescriptor.Electricity;
+                });
+                bp.AddComponent<BuffDescriptorImmunity>(c => {
+                    c.Descriptor = SpellDescriptor.Fire;
+                });
+                bp.AddComponent<BuffDescriptorImmunity>(c => {
+                    c.Descriptor = SpellDescriptor.Sonic;
+                });
+                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
+                    c.Descriptor = SpellDescriptor.Acid;
+                });
+                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
+                    c.Descriptor = SpellDescriptor.Cold;
+                });
+                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
+                    c.Descriptor = SpellDescriptor.Electricity;
+                });
+                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
+                    c.Descriptor = SpellDescriptor.Fire;
+                });
+                bp.AddComponent<SpellImmunityToSpellDescriptor>(c => {
+                    c.Descriptor = SpellDescriptor.Sonic;
+                });
             });
         }
     }
